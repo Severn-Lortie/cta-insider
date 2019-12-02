c1df3490b6d5c3acbad6e829f9d8e7a5e28c38b6
Severn Lortie <severn@Severns-MacBook-Pro.local>
2019-12-02 00:59:49 -0500

    fixed 'undefined' article links from section list

--- src/components/article-list/ArticleList.vue
+++ src/components/article-list/ArticleList.vue
@@ -1,16 +1,14 @@
 <template>
 <base-article-list
     :section="section"
-    :sectioId="sectionId"
+    :sectionId="sectionId"
 >
     <base-article-list-load-button v-on:load-more="fetchNextArticles()"></base-article-list-load-button>
 </base-article-list>
 </template>
 
 <script>
-import {
-    mapState
-} from 'vuex'
+import {mapState} from 'vuex'
 import stringToKey from '../../util/stringToKey'
 import BaseArticleList from './BaseArticleList'
 import BaseArticleListLoadButton from './BaseArticleListLoadButton';
